11fdbca62dc145c55c167ebb4864286fcd1b696a
FranciscoJoseBonet Desktop <franciscojosebonet@gmail.com>
2025-03-10 16:15:28 -0300

    Creacion de funciones para eliminacion y agregado de registro a las listas

--- JS/app.js
+++ JS/app.js
@@ -11,6 +11,11 @@ class registro {
 		this.descripcion = descripcion;
 		this.esMensual = esMensual;
 		this.fecha = new Date().toISOString().split("T")[0];
+		if (this.tipo === "ingreso") {
+			this.id = ingresos.length + 1;
+		} else {
+			this.id = gastos.length + 1;
+		}
 	}
 
 	//No se si sirva pero creamos una forma de eliminar el registro de la memoria
@@ -28,7 +33,7 @@ class registro {
 	}
 }
 
-//Almacenamiento de clases que despues convertire a JSON
+//Almacenamiento (Pasar a JSON)
 gastos = [];
 ingresos = [];
 
@@ -71,20 +76,36 @@ function ingresarRegistro() {
 
 	//Creamos el registro
 	const ultRegistro = new registro(tipo, monto, descripcion, esMensual);
+	agregarRegistro(ultRegistro);
 }
 
 //Agregar un registro a las listas
 function agregarRegistro(registro) {
 	if (registro.tipo === "ingreso") {
-		ingreso.push(registro);
+		ingresos.push(registro);
+	} else {
+		gastos.push(registro);
 	}
 }
 
 //Eliminar un registro de las listas
-function eliminarRegistro(registro) {}
+function eliminarRegistro(registro) {
+	if (registro.tipo === "ingreso") {
+		ingresos = ingresos.filter((reg) => reg.id !== registro.id);
+	} else {
+		gastos = gastos.filter((reg) => reg.id !== registro.id);
+	}
+}
 
 //Editar un registro de las listas
 function editarRegistro(registro) {}
 
 //Main de prueba
-ingresarRegistro();
+registro1 = new registro("ingreso", 1000, "Sueldo", true);
+agregarRegistro(registro1);
+registro2 = new registro("ingreso", 5000, "aaaaaa", false);
+agregarRegistro(registro2);
+registro3 = new registro("ingreso", 1000, "Sueldo", true);
+agregarRegistro(registro3);
+
+console.log(ingresos);
