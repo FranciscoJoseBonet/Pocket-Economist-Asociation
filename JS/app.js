c1d6c0f254293faacdde7ef3d5384186915f8df1
FranciscoJoseBonet Desktop <franciscojosebonet@gmail.com>
2025-03-10 15:40:56 -0300

    Funcion para cargar el registro creada

--- JS/app.js
+++ JS/app.js
@@ -33,10 +33,58 @@ gastos = [];
 ingresos = [];
 
 //Pedirle al ususario que ingrese un registro
-function pedirRegistro() {}
+function ingresarRegistro() {
+	let aux;
+	//tipo
+	do {
+		aux = Number(
+			prompt("Escriba 1 para declarar un INGRESO o 2 para declarar un GASTO")
+		);
+		if (aux !== 1 && aux !== 2) {
+			alert("Por favor, ingrese un numero válido");
+		}
+	} while (aux !== 1 && aux !== 2);
+	const tipo = aux === 1 ? "ingreso" : "gasto";
+
+	//monto
+	do {
+		aux = Number(prompt(`Ingrese el monto del ${tipo}`));
+		if (isNaN(aux)) {
+			alert("Por favor, ingrese un valor numérico");
+		}
+	} while (isNaN(aux));
+	const monto = aux;
+
+	//descripcion
+	if (confirm(`Desea agregar una descripcion al ${tipo}?`)) {
+		aux = prompt(`Ingresa una descripcion para el ${tipo}`);
+	} else {
+		aux = "No posee una descripcion asignada";
+	}
+	const descripcion = aux;
+
+	//esMensual
+	aux = confirm(
+		`El ${tipo} es mensual? Presiona Aceptar para SI o Cancelar para NO`
+	);
+	const esMensual = aux;
+
+	//Creamos el registro
+	const ultRegistro = new registro(tipo, monto, descripcion, esMensual);
+}
 
 //Agregar un registro a las listas
-function agregarRegistro(registro) {}
+function agregarRegistro(registro) {
+	if (registro.tipo === "ingreso") {
+		ingreso.push(registro);
+	}
+}
 
 //Eliminar un registro de las listas
 function eliminarRegistro(registro) {}
+
+//Editar un registro de las listas
+function editarRegistro(registro) {}
+
+//Main de prueba
+ingresarRegistro();
