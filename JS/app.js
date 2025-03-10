8ac0c88cab10725816f3bd223837b5e6fe174bfd
FranciscoJoseBonet Desktop <franciscojosebonet@gmail.com>
2025-03-10 14:56:04 -0300

    Agregado de un metodo para olvidar registros y creacion de funciones de interaccion de los registros con sus arrays

--- JS/app.js
+++ JS/app.js
@@ -1,19 +1,42 @@
-// Clase que se encarga de manejar los registros de gastos e ingresos
+//Registros de gastos e ingresos
 class registro {
 	constructor(
 		tipo,
 		monto,
 		descripcion = "No posee una descripcion asignada",
-		esFijo = false
+		esMensual = false
 	) {
 		this.tipo = tipo;
 		this.monto = monto;
 		this.descripcion = descripcion;
-		this.esFijo = esFijo;
+		this.esMensual = esMensual;
 		this.fecha = new Date().toISOString().split("T")[0];
 	}
 
+	//No se si sirva pero creamos una forma de eliminar el registro de la memoria
+	destruir() {
+		this.tipo = null;
+		this.monto = null;
+		this.descripcion = null;
+		this.esMensual = null;
+		this.fecha = null;
+	}
+
+	//Para probar el console.log()
 	mostrarRegistro() {
-		return `tipo :${this.tipo}, monto: ${this.monto}, descripcion: ${this.descripcion}, esFijo: ${this.esFijo}, fecha: ${this.fecha}`;
+		return `tipo :${this.tipo}, monto: ${this.monto}, descripcion: ${this.descripcion}, esMensual: ${this.esMensual}, fecha: ${this.fecha}`;
 	}
 }
+
+//Almacenamiento de clases que despues convertire a JSON
+gastos = [];
+ingresos = [];
+
+//Pedirle al ususario que ingrese un registro
+function pedirRegistro() {}
+
+//Agregar un registro a las listas
+function agregarRegistro(registro) {}
+
+//Eliminar un registro de las listas
+function eliminarRegistro(registro) {}
